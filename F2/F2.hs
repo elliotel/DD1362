635eac2e6ce8c887a74fce439ebdf87f8504118c
elliotel <75089214+elliotel@users.noreply.github.com>
2024-02-15 05:17:00 +0100

    Add final changes to F2
    Added final changes to F2 to pass all tests

--- F2/F2.hs
+++ F2/F2.hs
@@ -1,46 +1,46 @@
+module F2 where
 import Data.List
 
 class Evol a where
     distance :: a -> a -> Double
     name :: a -> String
     distanceMatrix :: [a] -> [(String, String, Double)]
-    distanceMatrix list = [(name a, name b, distance a b) | a <- list, b <- list, name a <= name b]
+    distanceMatrix [] = []
+    distanceMatrix list = [(name a, name b, distance a b) | (a, i) <- listWithIndexes, (b, j) <- listWithIndexes, i <= j]
+        where
+        listWithIndexes = zip list [0..]
 
 
 data MolSeq = Sequence String String String
 
 instance Evol MolSeq where
-    distance :: MolSeq -> MolSeq -> Double
     distance = seqDistance
-    name :: MolSeq -> String
     name = seqName
 
-data Profile = Prof [[(Char, Int)]] String Int String
+data Profile = Prof [[(Char, Double)]] String Int String
 
 instance Evol Profile where
-    distance :: Profile -> Profile -> Double
     distance = profileDistance
-    name :: Profile -> String
     name = profileName
 
 
 instance Show MolSeq where
-    show :: MolSeq -> String
     show (Sequence name seq variation) = "Molecular sequence: " ++ name ++ "\nType: " ++ variation ++ "\nSequence: " ++ seq ++ "\nLength: " ++ show (seqLength (Sequence name seq variation)) ++ "\n"
 
 instance Show Profile where
-    show :: Profile -> String
     show (Prof matrix variation sequenceAmount name)  = "Profile: " ++ name ++ "\nType: " ++ variation ++ "\nSequences: " ++ show sequenceAmount ++ "\nMatrix: " ++ show matrix ++ "\n"
 
+
+{-
 main :: IO ()
 main = do
-    -- Testing for the sequence part of the assignment begin here.
-    let dnaMolecule = string2seq "TestDNA" "AAGTCCGTAGA"
-    let dnaMolecule2 = string2seq "TestDNA2" "AAGTCAGTAGA"
-    let dnaMolecule3 = string2seq "TestDNA3" "GGAGGGAGGAG"
-    let proteinMolecule = string2seq "TestProtein" "AAGTCEGTAGA"
-    let proteinMolecule2 = string2seq "TestProtein2" "AAETCAGTAEA"
-    let proteinMolecule3 = string2seq "TestProtein3" "EEEEEEEEEEE"
+    -- Testing for the sequence part of the assignment begins here.
+    let dnaMolecule = string2seq "A" "AAGTCCGTAGA"
+    let dnaMolecule2 = string2seq "B" "AAGTCAGTAGA"
+    let dnaMolecule3 = string2seq "C" "GGAGGGAGGAG"
+    let proteinMolecule = string2seq "A" "AAGTCEGTAGA"
+    let proteinMolecule2 = string2seq "B" "AAETCAGTAEA"
+    let proteinMolecule3 = string2seq "C" "EEEEEEEEEEE"
     print dnaMolecule
     print dnaMolecule2
     print dnaMolecule3
@@ -52,8 +52,8 @@ main = do
     print ("Distance between " ++ name proteinMolecule ++ " and " ++ name proteinMolecule2 ++ ": " ++ show (seqDistance proteinMolecule proteinMolecule2))
     print ("Distance between " ++ name proteinMolecule ++ " and " ++ name proteinMolecule3 ++ ": " ++ show (seqDistance proteinMolecule proteinMolecule3))
     putStrLn ""
-    -- Testing for the profile part of the assignment begin here.
-    let dnaMolecule4 = string2seq "TestDNA4" "GGTACAGATAC"
+    -- Testing for the profile part of the assignment begins here.
+    let dnaMolecule4 = string2seq "D" "GGTACAGATAC"
     let dnaMolecule5 = string2seq "TestDNA5" "AGTTTACGATA"
     let dnaMolecule6 = string2seq "TestDNA6" "CCCCTAGGGAT"
     let dnaMolecule7 = string2seq "TestDNA7" "TTAGGACATAC"
@@ -62,7 +62,7 @@ main = do
     let dnaProfile = molseqs2profile "DNA Profile 1" [dnaMolecule, dnaMolecule2, dnaMolecule3]
     let dnaProfile2 = molseqs2profile "DNA Profile 2" [dnaMolecule4, dnaMolecule5, dnaMolecule6]
     let dnaProfile3 = molseqs2profile "DNA Profile 3" [dnaMolecule7, dnaMolecule8, dnaMolecule9]
-    let proteinMolecule4 = string2seq "TestProtein4" "GETACAIATDC"
+    let proteinMolecule4 = string2seq "D" "GETACAIATDC"
     let proteinMolecule5 = string2seq "TestProtein5" "AGTTTEIGATE"
     let proteinMolecule6 = string2seq "TestProtein6" "CEECTAGDQAT"
     let proteinMolecule7 = string2seq "TestProtein7" "TTAGEECATAC"
@@ -71,6 +71,7 @@ main = do
     let proteinProfile = molseqs2profile "Protein Profile 1" [proteinMolecule, proteinMolecule2, proteinMolecule3]
     let proteinProfile2 = molseqs2profile "Protein Profile 2" [proteinMolecule4, proteinMolecule5, proteinMolecule6]
     let proteinProfile3 = molseqs2profile "Protein Profile 3" [proteinMolecule7, proteinMolecule8, proteinMolecule9]
+    
 
     print dnaProfile
     print dnaProfile2
@@ -81,13 +82,30 @@ main = do
     
     
 
-    print (distanceMatrix [dnaMolecule, dnaMolecule2, dnaMolecule3])
+    print (distanceMatrix [dnaMolecule, dnaMolecule2, dnaMolecule3, dnaMolecule4])
     putStrLn ""
-    print (distanceMatrix [proteinMolecule, proteinMolecule2, proteinMolecule3])
+    print (distanceMatrix [proteinMolecule, proteinMolecule2, proteinMolecule3, proteinMolecule4])
     putStrLn ""
-    print (distanceMatrix [dnaProfile, dnaProfile2, dnaProfile3])
+    print (distanceMatrix [dnaMolecule, dnaMolecule2])
     putStrLn ""
-    print (distanceMatrix [proteinProfile, proteinProfile2, proteinProfile3])
+    print (distanceMatrix [proteinMolecule, proteinMolecule2])
+    putStrLn ""
+    print (distanceMatrix [dnaMolecule])
+    putStrLn ""
+    print (distanceMatrix [proteinMolecule])
+    putStrLn ""
+    --print (distanceMatrix ([] :: [MolSeq]))
+    putStrLn ""
+    --print (distanceMatrix ([] :: [Profile]))
+    putStrLn ""
+    print (distanceMatrix [dnaMolecule3, dnaMolecule2, dnaMolecule])
+    putStrLn ""
+    print (distanceMatrix [proteinMolecule3, proteinMolecule2, proteinMolecule])
+    putStrLn ""
+    --print (distanceMatrix [dnaProfile, dnaProfile2, dnaProfile3])
+    --putStrLn ""
+    --print (distanceMatrix [proteinProfile, proteinProfile2, proteinProfile3])
+-}
 
 string2seq :: String -> String -> MolSeq
 string2seq name seq = Sequence name seq (typeHelper seq)
@@ -124,7 +142,7 @@ seqDistance seq1 seq2 = distance
                     else let (const1, const2, const3) | isDNA = (3/4, 4, 3)
                                                       | otherwise = (19/20, 20, 19)
 
-                             threshold = if isDNA then 0.74 else 0.4
+                             threshold = if isDNA then 0.74 else 0.94
                              result 
                                 | alpha > threshold && isDNA = 3.3
                                 | alpha > threshold = 3.7
@@ -142,20 +160,20 @@ nucleotides :: String
 nucleotides = "ACGT"
 aminoacids :: [Char]
 aminoacids = sort "ARNDCEQGHILKMFPSTWYV"
-makeProfileMatrix :: [MolSeq] -> [[(Char, Int)]]
+makeProfileMatrix :: [MolSeq] -> [[(Char, Double)]]
 makeProfileMatrix [] = error "Empty sequence list"
-makeProfileMatrix sl = res
+makeProfileMatrix mols = matrix
     where
-        t = seqType (head sl) -- Sparar typen av Molseq, dvs DNA eller Protein
+        t = seqType (head mols) -- Sparar typen av Molseq, dvs DNA eller Protein
         defaults =
             if t == "DNA" then
                 zip nucleotides (replicate (length nucleotides) 0) -- Om typen är DNA, matcha alla nucleotides (ACTG) med nollor i tuplar, dvs [('A', 0), ('C', 0), ('G', 0), ('T', 0)], och sparar detta som defaults.
             else
                 zip aminoacids (replicate (length aminoacids) 0) -- Om typen är Protein, utför samma process som ovan fast för alla aminoacids (ARNDCEQGHILKMFPSTWYV) och spara som tuplar med 0.
-        seqs = map seqSequence sl -- Kör funktionen seqSequence på varje element i s1 och sparar som ny lista, seqs, vilken alltså blir en lista med alla sekvenser som strängar.
+        seqs = map seqSequence mols -- Kör funktionen seqSequence på varje element i s1 och sparar som ny lista, seqs, vilken alltså blir en lista med alla sekvenser som strängar.
         transp = transpose seqs {- Om vi ser på vår lista av sekvenser som en matris (då strängarna i sig är listor), så transponerar detta rader och kolumner. 
         Det innebär att vi kommer få en ny lista med de första elementen i varje string ur den urprungliga listan, som den första string:en i den nya listan, de andra elementen i varje string som den andra stringen osv. -}
-        func x = (head x, length x) -- Tar en lista och returnerar en tuple som innehåller det första elementet i listan, samt listans längd. Exempelvis blir [5, 2, 3, 4] till (5, 4)
+        func x = (head x, fromIntegral (length x) / fromIntegral (length mols)) -- Tar en lista och returnerar en tuple som innehåller det första elementet i listan, samt listans längd. Exempelvis blir [5, 2, 3, 4] till (5, 4)
         sortAndGroup = map func . group . sort {- Tar en lista, och sorterar den först. I vårt fall kommer detta vara string:s med alla elementen som ursprungligen var på en viss position, dvs de string:s som skapades av transponeringen.
         Funktionen group grupperar sedan vidare de intilliggande element som är lika i ytterligare sublistor. Exempelvis "AAG" = ['A', 'A', 'G'] -> [['A', 'A'], ['G']] = ["AA", "G"].
         Funktionen func körs sedan på varje lista i detta resultat, vilket returnerar en tuple med de första element i listan, samt längden på listan. För exempel ovan får vi då ["AA", "G"] -> [('A', 2), ('G', 1)]. -}
@@ -164,8 +182,9 @@ makeProfileMatrix sl = res
         Detta ger oss alltså en lista, som genom sublistor av tuplar säger hur många av varje element som befinner sig på varje position. [('A', 2), ('G', 1)] betyder till exempel att på den första positionen i alla sekvenserna finns 2 A och 1 G, osv. -}
         equalFst a b = fst a == fst b -- Funktion som returnerar true eller false beroende på om de första elementen i tuple a och tuple b är lika.
         combineTuples list = unionBy equalFst list defaults -- Kombinerar tuplarna i defaults, med tuplarna i den försedda list, då det första elementet i tuplarna, i vårt fall bokstaven, är samma.
-        res = map (sort . combineTuples) elementsByPosition {- Kör combineTuples samt sorterar resultatet på varje lista i elementsByPosition, sorteringsex: [('A',2),('G',1),('C',0)] blir [('A',2),('C',0),('G',1)].
+        undividedMatrix = map (sort . combineTuples) elementsByPosition {- Kör combineTuples samt sorterar resultatet på varje lista i elementsByPosition, sorteringsex: [('A',2),('G',1),('C',0)] blir [('A',2),('C',0),('G',1)].
         Resultatet blir nu en lista av listor (matris) av tuples, som säger oss hur många ev ett visst element / viss bokstav som finns på en viss position (exempelvis första bokstaven) i alla sekvenserna vi började med. -}
+        matrix = undividedMatrix
         
 
 molseqs2profile :: String -> [MolSeq] -> Profile
@@ -182,10 +201,10 @@ profileName (Prof _ _ _ name) = name
 profileFrequency :: Profile -> Int -> Char -> Double
 profileFrequency (Prof matrix _ _ _) pos char = findFrequency (matrix !! pos) -- Tar listan på position pos i matrisen matrix
     where
-        findFrequency :: [(Char, Int)] -> Double
+        findFrequency :: [(Char, Double)] -> Double
         findFrequency [] = error "Character not part of any sequence!"
         findFrequency ((c, freq):xs) -- Går igenom varje tuple i listan
-            | c == char = fromIntegral freq -- Om bokstaven stämmer med char så tar den det andra värdet ur tuplen och returnerar det som frekvensen
+            | c == char = freq -- Om bokstaven stämmer med char så tar den det andra värdet ur tuplen och returnerar det som frekvensen
             | otherwise = findFrequency xs -- Annars fortsätter den att rekursivt gå igenom alla tuples
 
 profileDistance :: Profile -> Profile -> Double
@@ -193,12 +212,12 @@ profileDistance (Prof matrix1 variation1 _ _) (Prof matrix2 variation2 _ _)
     | variation1 /= variation2 = error "Profiles are of different molecule types"
     | otherwise = distance
     where
-        sumTuples :: [(Char, Int)] -> [(Char, Int)] -> Double
+        sumTuples :: [(Char, Double)] -> [(Char, Double)] -> Double
         sumTuples [] [] = 0.0
         --Eftersom våra listor är sorterade behöver vi inte tänka på vilken karaktär vi är vid, de kommer alltid att vara samma över båda listorna.
-        sumTuples ((_, freq1):xs1) ((_, freq2):xs2) = abs (fromIntegral (freq1 - freq2)) + sumTuples xs1 xs2
+        sumTuples ((_, freq1):xs1) ((_, freq2):xs2) = abs (freq1 - freq2) + sumTuples xs1 xs2
 
-        sumFrequency :: [[(Char, Int)]] -> [[(Char, Int)]] -> Double
+        sumFrequency :: [[(Char, Double)]] -> [[(Char, Double)]] -> Double
         sumFrequency [] [] = 0.0
         sumFrequency (x1:xs1) (x2:xs2) = sumTuples x1 x2 + sumFrequency xs1 xs2
 
