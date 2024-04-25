8554d37bc99ba6d3fef502daf10bd5ca3f677c52
elliotel <75089214+elliotel@users.noreply.github.com>
2024-04-25 22:47:08 +0200

    Remove unused imports

--- S2/Lexer.java
+++ S2/Lexer.java
@@ -1,9 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class Lexer {
 
@@ -67,8 +64,4 @@ public class Lexer {
     
         return tokens;
     }
-    
-    
-
-
 }
